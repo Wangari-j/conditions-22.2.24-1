e7a8e94c435ca13a4688aa75fd961a9e58e107ca
Wangari-j <wangarijudy@gmail.com>
2024-02-22 05:24:56 +0300

    conditions

---
+++ conditions.py
@@ -0,0 +1,32 @@
+#python if statement
+
+number = 10
+
+if number > 5:
+    print("Number is greater")
+
+number1 = -5
+
+if number1 > 0:
+    print("A positive number")
+
+print('This statement always executes.')
+
+#if...else
+#else executes if the condition in the if statement evaluates to false
+
+if 5 >10:
+    print('5 is greater than 10')
+
+else:
+    print('10 is greater than 5')
+
+
+num = 20
+if num >=40:
+    print('Is greater than 40')
+
+else:
+    print('Is less than 40')
+
+print('This statement always executes')
